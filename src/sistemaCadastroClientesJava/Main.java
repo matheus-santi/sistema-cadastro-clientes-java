0bec70b5a2e93bbb93375a63287b20f3d44d7359
matheus-santi <146378904+matheus-santi@users.noreply.github.com>
2026-02-23 16:24:02 -0300

    Estrutura inicial + classe cliente pronta do projeto

--- src/sistemaCadastroClientesJava/Main.java
+++ src/sistemaCadastroClientesJava/Main.java
@@ -1,9 +1,15 @@
 package sistemaCadastroClientesJava;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner("System.in");
+		// a ser implementado...
+		
+		
+		scan.close();
 
 	}
 
--- src/sistemaCadastroClientesJava/clienteService.java
+++ src/sistemaCadastroClientesJava/clienteService.java
@@ -1,5 +1,8 @@
 package sistemaCadastroClientesJava;
 
+import java.util.ArrayList;
+
 public class clienteService {
+	private ArrayList<Cliente> listaClientes;
 
 }
