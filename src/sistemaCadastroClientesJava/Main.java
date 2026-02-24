f16a6935505db0a0270abbd3f3c5c101f25f59a5
matheus-santi <146378904+matheus-santi@users.noreply.github.com>
2026-02-24 15:16:50 -0300

    Menu principal implementado

--- src/sistemaCadastroClientesJava/Main.java
+++ src/sistemaCadastroClientesJava/Main.java
@@ -5,12 +5,61 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner("System.in");
-		// a ser implementado...
+		Scanner scan = new Scanner(System.in);
+		int opcao;
+		
+		do {
+			System.out.println(":::SISTEMA DE CADASTRO DE CLIENTES:::");
+			System.out.println("Escolha uma opção:  "
+					           + "\n1 - Cadastrar\r\n"
+					           + "2 - Listar\r\n"
+					           + "3 - Editar\r\n"
+					           + "4 - Remover\r\n"
+					           + "0 - Sair\n:");
+			opcao = scan.nextInt();
+			
+			switch (opcao) {
+			case 1: 
+				cadastrar();
+				break;
+			case 2:
+				listar();
+				break;
+			case 3:
+				editar();
+				break;
+			case 4:
+				remover();
+				break;
+			case 0:
+				System.out.println("\nSaindo do sistema...");
+				break;
+				
+				default:
+					System.out.println("Opção invalida. Tente novamente!!");
+			}
+			
+		} while (opcao != 0);
 		
 		
 		scan.close();
 
+	}
+	
+	public static void cadastrar() {
+		//...
+	}
+	
+    public static void listar() {
+		//...
+	}
+    
+    public static void editar() {
+		//...
+	}
+    
+    public static void remover() {
+		//...
 	}
 
 }
